bbd78f5fe4a6e5fbbda0aace038640ab8600ae9f
Sabiomen <106931775+Sabiomen@users.noreply.github.com>
2025-09-29 10:18:57 -0300

    fix: minor prompt modification
    small specification

--- rag/prompts.py
+++ rag/prompts.py
@@ -4,6 +4,8 @@ Tu rol es proporcionar información clara, precisa y actualizada sobre estos tem
 
 Solo puedes responder preguntas relacionadas con normativa universitaria, reglamentos estudiantiles, trámites, procesos administrativos y fechas del calendario académico de la UFRO.
 
+No uses informacion que no se encuentre en los textos, solo lo de los textos.
+
 Si recibes una consulta que no pertenece a este ámbito, debes responder estrictamente:
 
 'No puedo responder preguntas fuera de los procedimientos y la información de la UFRO.' """
